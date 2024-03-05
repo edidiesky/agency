a7d5c2c51e245f5870d710abce97268ded90d497
edidiesky <essienedidiong1000@gmail.com>
2024-03-05 04:21:14 -0800

    new fixes

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 import Hero from './_components/Hero'
+import { AnimatePresence, motion } from 'framer-motion'
 import { Header } from '@/components/common/Header';
 import Menu from '@/components/common/Menu';
 import { useState } from 'react';
@@ -10,10 +11,15 @@ export default function Home() {
   const [menu, setMenu] = useState(false)
   return (
     <>
+      <AnimatePresence>
+        <Menu menu={menu} setMenu={setMenu} />
+      </AnimatePresence>
       <div className="w-full bg-[#000] font-Agency_medium min-h-full relative">
+
         <Header
           menu={menu}
           setMenu={setMenu}
+          type={true}
         />
 
         <Hero />
--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -13,29 +13,32 @@ export const Header: React.FC<HeaderProps> = ({ menu, setMenu, type }) => {
   return (
     <div className="py-8 pb-5 fixed w-full md:px-12 top-0 z-40">
       <div className="w-[90%] m-auto max-w-custom flex items-center justify-between">
-        <h2 className={`text-2xl md:text-4xl ${type ? 'text-[#000]' : 'text-[#Fff]'}`}>
-          <span className="font-Agency_Extended">ONE: </span> NILL</h2>
+        <h2 className={`text-2xl md:text-4xl ${type ? 'text-[#fff]' : 'text-[#Fff]'}`}>
+          <span className="font-Agency_Extended">ONE: </span> <span style={{letterSpacing:"3px"}} className="text-light font-Agency_light">NILL</span></h2>
         <div className="flex items-center gap-4">
-          <div
-            onMouseLeave={() => setActive(false)}
-            onMouseOver={() => setActive(true)}
+          {
+            !type && <div
+              onMouseLeave={() => setActive(false)}
+              onMouseOver={() => setActive(true)}
 
-            className="h-14 cursor-pointer relative overflow-hidden min-w-52 rounded-[40px] text-lg bg-transparent text-text_dark_1 font-Agency_medium font-normal">
-            <motion.div
-              animate={{ top: active ? '-100%' : "0" }}
-              transition={{ duration: .5, ease: [0.75, 0, 0.24, 1] }}
-              className="flex relative h-full min-w-52 flex-col rounded-[40px]">
-              <Button
+              className="h-14 cursor-pointer relative overflow-hidden min-w-52 rounded-[40px] text-lg bg-transparent text-text_dark_1 font-Agency_medium font-normal">
+              <motion.div
+                animate={{ top: active ? '-100%' : "0" }}
+                transition={{ duration: .5, ease: [0.75, 0, 0.24, 1] }}
+                className="flex relative h-full min-w-52 flex-col rounded-[40px]">
+                <Button
 
-                className={`h-full w-full ${type ? 'bg-[#000] text-white' : 'bg-[#Fff] text-text_dark_1'} font-normal text-base font-Agency_medium rounded-[40px] text-center`}>
-                Get in Touch
-              </Button>
-              <Button className={`h-full w-full ${type ? 'bg-[#000] text-white' : 'bg-[#000] text-white'} 
+                  className={`h-full w-full ${type ? 'bg-[#000] text-white' : 'bg-[#Fff] text-text_dark_1'} font-normal text-base font-Agency_medium rounded-[40px] text-center`}>
+                  Get in Touch
+                </Button>
+                <Button className={`h-full w-full ${type ? 'bg-[#000] text-white' : 'bg-[#000] text-white'} 
             absolute top-[100%] left-0 font-normal text-base font-Agency_medium rounded-[40px] text-center`}>
-                Visit Our Works
-              </Button>
-            </motion.div>
-          </div>
+                  Visit Our Works
+                </Button>
+              </motion.div>
+            </div>
+          }
+
           {/*  */}
           {
             type ? <div onClick={() => setMenu(!menu)} className={"icons w-12 h-12 cursor-pointer text-text_dark_1 text-xl md:text-3xl hover:bg-[rgba(180,180,180,.2)] hover:transform hover:-translate-y-1 transition ease-in duration-300 rounded-full flex items-center justify-center"}>
