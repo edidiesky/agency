602001160094c67aa805695bc3c8d31618b08aee
edidiesky <essienedidiong1000@gmail.com>
2024-03-05 03:12:15 -0800

    new fixes

--- app/(Home)/_components/Works.tsx
+++ app/(Home)/_components/Works.tsx
@@ -4,12 +4,13 @@ import React, { useEffect, useRef } from "react";
 import SplitType from "split-type";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import Link from "next/link";
 const Works = () => {
     const titleref = useRef(null);
     const titleRef_2 = useRef(null);
     const titleRef_3 = useRef(null);
     const imageRef = useRef<HTMLDivElement[]>([]);
-    const descriptionsRef = useRef < HTMLDivElement[]>([]);
+    const descriptionsRef = useRef<HTMLDivElement[]>([]);
     imageRef.current = []
     descriptionsRef.current = []
 
@@ -138,16 +139,16 @@ const Works = () => {
             </div>
             <div className="w-full grid grid-cols-1 md:grid-cols-3 gap-4">
 
-                <div ref={AddImageToRefs} data-scroll-speed={"3"} className="w-full lg:mt-24 relative h-[35rem] flex items-end">
+                <Link  href={'/work/1'} ref={AddImageToRefs} data-scroll-speed={"3"} className="w-full lg:mt-24 relative h-[35rem] flex items-end">
                     <img src="https://onenil.com/media/pages/work/heineken-cheers-to-you/03bf898036-1686575712/schermafbeelding-2023-06-12-om-15.14.56.png"
                         alt="" className="absolute h-full object-cover w-full z-10" />
                     <div className="w-full flex items-start z-20 pt-10 pb-20 px-8 justify-start flex-col gap-4">
                         <h3 className="text-3xl font-normal font-Agency_medium leading-[1.3] text-white">Ajax takes a stand against racism</h3>
                         <h5 className="text-sm font-normal font-Agency_light leading-[1.3] text-text_grey_1">View Case</h5>
                     </div>
-                </div>
+                </Link>
 
-                <div ref={AddImageToRefs} data-scroll-speed={"1.6"} className="w-full lg:mt-16 relative h-[35rem] flex items-end">
+                <Link href={'/work/1'} ref={AddImageToRefs} data-scroll-speed={"1.6"} className="w-full lg:mt-16 relative h-[35rem] flex items-end">
                     <div className="absolute h-full w-full bg-[rgba(0,0,0,.7)] z-20"></div>
 
                     {/* https://onenil.com/media/pages/work/future-goals/cd6dec9cdb-1685445528/01.png */}
@@ -158,9 +159,9 @@ const Works = () => {
                         <h3 className="text-3xl font-normal font-Agency_medium leading-[1.3] text-white">From making fishing nets to football goals</h3>
                         <h5 className="text-sm font-normal font-Agency_light leading-[1.3] text-text_grey_1">View Case</h5>
                     </div>
-                </div>
+                </Link>
 
-                <div ref={AddImageToRefs} data-scroll-speed={"2.6"} className="w-full lg:mt-8 relative h-[35rem] flex items-end">
+                <Link href={'/work/1'} ref={AddImageToRefs} data-scroll-speed={"2.6"} className="w-full lg:mt-8 relative h-[35rem] flex items-end">
                     <div className="absolute h-full w-full bg-[rgba(0,0,0,.1)] z-10"></div>
 
                     <img src="https://onenil.com/media/pages/home/15159f1d0c-1685445527/on-2021-eredivisie-otw-collage-1920x1080-q72.jpg"
@@ -170,7 +171,7 @@ const Works = () => {
                         <h3 className="text-3xl font-normal font-Agency_medium leading-[1.2] text-white">Ajax celebrate a much Champoins Cup</h3>
                         <h5 className="text-sm font-normal font-Agency_light leading-[1.3] text-text_grey_1">View Case</h5>
                     </div>
-                </div>
+                </Link>
             </div>
         </div>
 
---
+++ app/contact/_components/Hero.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+const Hero = () => {
+    return <div data-scroll style={{ minHeight: "100vh" }} className=' relative w-full flex items-center justify-center'>
+        <div style={{ background: "linear-gradient(0deg, rgba(0,0,0,1) 0%, rgba(0,0,0,.2) 100%)" }} className="absolute h-full w-full z-10"></div>
+        <img style={{ minHeight: "100vh", zIndex: 5 }} src="https://onenil.com/media/pages/work/future-goals/198accd6a3-1685445528/news-1-ajax-and-sandals-1920x1080-q72.jpg" alt=""
+            className="object-cover absolute h-full w-full" />
+
+        <div className="flex flex-col gap-12 h-full w-full">
+            <div className="h-full z-20 pt-40 pb-20 w-[90%] max-w-custom_1 mx-auto grid items-start justify-center grid-cols-custom_2 gap-8">
+                <div className="w-full">
+                    <h4 className="lg:text-xl z-20 font-normal text-white uppercase font-Agency_Extended">
+                        case
+
+                    </h4>
+                </div>
+                <div className="flex flex-col items-start gap-8">
+                    <h1 className="text-4xl lg:text-5xl z-20 font-normal text-white uppercase font-Agency_Extended">
+                        FROM FISHING NETS TO FOOTBALL GOALS
+                    </h1>
+                    <div className="pt-16 w-full flex gap-4">
+                        <div className="flex flex-col gap-4">
+                            <span className="block text-base text-text_grey_1 w-[90%] font-light font-Agency_light">
+                                CLIENT
+                            </span>
+                            <span className="block text-base text-white w-[90%] font-light font-Agency_light">
+                                Sandals Resorts
+                            </span>
+                        </div>
+
+                        <div className="flex flex-col gap-4">
+                            <span className="block text-base text-text_grey_1 w-[90%] font-light font-Agency_light">
+                                SERVICE
+                            </span>
+                            <span className="block text-base text-white w-[90%] font-light font-Agency_light">
+                                Strategy, Concept, Campaign, Design, Video, Social
+                            </span>
+                        </div>
+
+                        <div className="flex flex-col gap-4">
+                            <span className="block text-base text-text_grey_1 w-[90%] font-light font-Agency_light">
+                                DATE
+                            </span>
+                            <span className="block text-base text-white w-[90%] font-light font-Agency_light">
+                                2022
+                            </span>
+                        </div>
+
+
+
+                    </div>
+                </div>
+
+            </div>
+
+
+            <div className="h-full z-20 pt-40 pb-32 w-[90%] max-w-custom_1 mx-auto items-start justify-center flex flex-col gap-8">
+
+                <div className="flex flex-col items-start gap-8">
+                    <h1 className="text-4xl lg:text-5xl z-20 font-normal text-white uppercase font-Agency_Extended">
+                        Ajax and Sandals Resorts join forces to facilitate football in The Caribbean
+                    </h1>
+                    <div className="pt-4 w-full flex gap-4">
+                        <span className="block text-base text-white w-[90%] font-light font-Agency_light">
+                            Children in Curaçao are celebrating new goals, as AFC Ajax and Sandals Resorts, together with its philanthropic arm, the Sandals Foundation, launch an island-wide program that creatively turns fishing nets sourced from the ocean and plastic waste into colorful football goals. Through the new Future Goals program, primary schools across the Dutch Caribbean island are set to receive new football equipment, accompanied by a sports program designed by the professional football team. With a common objective of empowering children through the game of football and providing the resources to play, the program marks the beginning of a collaboration between AFC Ajax and SRI, which is opening a new resort on the island.
+                        </span>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>;
+}
+export default Hero;
